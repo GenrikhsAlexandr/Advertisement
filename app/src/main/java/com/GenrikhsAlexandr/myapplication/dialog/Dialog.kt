95671eb56cbbe1f283561b8b0d73a4a9c09ee772
Aleksandr <aleksandr.genrikhs@gmail.com>
2022-12-15 16:54:05 +1100

    add recovery Password

--- app/src/main/java/com/GenrikhsAlexandr/myapplication/dialog/Dialog.kt
+++ app/src/main/java/com/GenrikhsAlexandr/myapplication/dialog/Dialog.kt
@@ -1,11 +1,13 @@
 package com.GenrikhsAlexandr.myapplication.dialog
 
 import android.app.AlertDialog
+import android.view.View
+import android.widget.Toast
+import androidx.core.view.isNotEmpty
 import com.GenrikhsAlexandr.myapplication.MainActivity
 import com.GenrikhsAlexandr.myapplication.R
 import com.GenrikhsAlexandr.myapplication.account.Account
 import com.GenrikhsAlexandr.myapplication.databinding.SignDialogBinding
-import com.google.android.material.textfield.TextInputLayout
 
 
 class Dialog(activity: MainActivity) {
@@ -18,6 +20,51 @@ class Dialog(activity: MainActivity) {
         val builder = AlertDialog.Builder(act)
         val binding = SignDialogBinding.inflate(act.layoutInflater)
         builder.setView(binding.root)
+
+        setDialogState(index, binding)
+
+        val dialog = builder.create()
+
+        binding.btSingInuP.setOnClickListener {
+            setOnClickSignUpIn(index, binding, dialog)
+        }
+
+        binding.btForgetP.setOnClickListener {
+            setOnClickForgetP(binding, dialog)
+        }
+
+
+        dialog.show()
+    }
+
+
+    private fun setOnClickForgetP(binding: SignDialogBinding, dialog: AlertDialog?) {
+        if(binding.edSignEmailLayout.editText?.text?.isNotEmpty() == true) {
+            act.mAuth.sendPasswordResetEmail(binding.edSignEmailLayout.editText?.text.toString())
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(act, R.string.password_recover, Toast.LENGTH_LONG).show()
+                    }
+                }
+            dialog?.dismiss()
+        }
+        else {
+            binding.tvRecoveryP.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setOnClickSignUpIn(index: Int, binding: SignDialogBinding, dialog: AlertDialog?) {
+        dialog?.dismiss()
+        if (index == Constants.SIGN_UP_STATE) {
+            acc.signUpWithEmail(binding.edSignEmailLayout.editText?.text.toString(), binding.edPasswordLayout.editText?.text.toString())
+
+        } else {
+            acc.signInWithEmail(binding.edSignEmailLayout.editText?.text.toString(), binding.edPasswordLayout.editText?.text.toString())
+        }
+
+    }
+
+    private fun setDialogState(index: Int, binding: SignDialogBinding) {
         if (index == Constants.SIGN_UP_STATE) {
             binding.tvSignTitle.text = act.resources.getString(R.string.ac_sign_up)
             binding.btSingInuP.text = act.resources.getString(R.string.sign_up_action)
@@ -25,22 +72,10 @@ class Dialog(activity: MainActivity) {
         } else {
             binding.tvSignTitle.text = act.resources.getString(R.string.ac_sign_in)
             binding.btSingInuP.text = act.resources.getString(R.string.sign_in_action)
-            binding.btForgetP.text = act.resources.getString(R.string.forget_password)
-        }
-        val dialog = builder.create()
-
-        binding.btSingInuP.setOnClickListener {
-            dialog.dismiss()
-            if (index == Constants.SIGN_UP_STATE) {
-                acc.signUpWithEmail(binding.edSignEmailLayout.editText?.text.toString(), binding.edPasswordLayout.editText?.text.toString())
-
-            } else {
-                acc.signInWithEmail(binding.edSignEmailLayout.editText?.text.toString(), binding.edPasswordLayout.editText?.text.toString())
-            }
-
+            binding.btForgetP.visibility = View.VISIBLE
         }
 
-
-        dialog.show()
     }
+
+
 }
